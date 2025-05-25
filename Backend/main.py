4096822b28bc1cde6e1daa983399add4d59cb02f
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-05-25 14:37:40 -0700

    Trying to get convertion to work

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,35 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Union, List
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+from pillow_heif import register_heif_opener
+import io
+import zipfile
+import pillow_heif
+import os
 
 app = FastAPI()
+register_heif_opener()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{items_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"items_id": item_id, "q": q}
+@app.post("/upload-and-convert-to-PNG")
+def convert_to_PNG(files: List[UploadFile] = File(...)):
+    convertedFiles = []
+    for file in files:
+        if file.lower().endswith(".heic"):
+            heif_file = pillow_heif.read_heif(files)
+            image = Image.frombytes(
+                heif_file.mode,
+                heif_file.size,
+                heif_file.data,
+                "raw",
+            )
+            new_filename = file.splittext(file)[0] + ".png"
+            convertedFiles.append(image.save(new_filename))
+
+
+    
+    return {files}
+
